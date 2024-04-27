019cebf5dfc2f83cb7827aaa9d1c341ad9ce4dc4
Wanja-Kayago <kayagonaomiwanja@gmail.com>
2024-04-27 15:39:23 +0300

    added files

--- constructors.js
+++ constructors.js
@@ -9,8 +9,8 @@ class Car {
         this.isAvailable = !this.isAvailable;
     }
 }
-const jeep = new Car('Jeep', 'SUV', 2017, 'true')
-console.log({jeep});
+const Toyota = new Car('Toyota', 'Honda', 2021, 'true')
+console.log({Toyota});
 // Rental class
 class Rental {
     constructor(car, renterName, rentalStartDate, rentalEndDate) {
@@ -25,11 +25,58 @@ class Rental {
         return differenceInDays;
     }
 }
-const uber = new Rental ('Audi', 'Maryann', "2024-4-12", "2024-6-6")
+const uber = new Rental ('Jeep', 'Frontier', "2024-4-27", "2024-13-12")
 console.log({uber});
 
 
+// Question 2
 
+class Question {
+    constructor(text, options, correctAnswer) {
+        this.text = text;
+        this.options = options;
+        this.correctAnswer = correctAnswer;
+    }
+    checkAnswer(userAnswer) {
+        return userAnswer === this.correctAnswer;
+    }
+}
+const sampleQuestion = new Question(
+    "What is the capital of France?",
+    ["Paris", "London", "Berlin", "Madrid"],
+   
+);
+console.log(sampleQuestion.checkAnswer("Paris"));
+console.log(sampleQuestion.checkAnswer("London"));
+class Quiz {
+    constructor() {
+        this.questions = [];
+        this.currentQuestionIndex = 0;
+        this.score = 0;
+    }
+
+    addQuestion(question) {
+        this.questions.push(question);
+    }
+    nextQuestion() {
+        this.currentQuestionIndex++;
+    }
+    submitAnswer(userAnswer) {
+        const currentQuestion = this.questions[this.currentQuestionIndex];
+        if (currentQuestion.checkAnswer(userAnswer)) {
+            this.score++;
+        }
+        this.nextQuestion();
+    }
+}
+const quiz = new Quiz();
+const question1 = new Question("What is the coding language is used for backend", ["Python", "Javascript", "DAS"], "Python");
+const question2 = new Question("Who is the best poet?", ["Shakespeare", "Hemingway", "Tolstoy"], "Shakespeare");
+quiz.addQuestion(question1);
+quiz.addQuestion(question2);
+quiz.submitAnswer("Python");
+quiz.submitAnswer("Shakespeare");
+console.log("Score:", quiz.score);
 
 
 
